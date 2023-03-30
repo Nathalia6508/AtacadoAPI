950fe865e970b5cc4edc43bd247c8298b35e7411
Nathalia6508 <102549857+Nathalia6508@users.noreply.github.com>
2023-03-29 21:37:16 -0400

    Add files via upload

---
+++ POO2/AtacadoConsole/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace AtacadoConsole;
+
+using Atacado.BD.EF.Database;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        AtacadoContext contexto = new AtacadoContext();
+
+        /*
+        foreach (Categorium item in contexto.Categoria.ToList())
+        {
+            Console.WriteLine($"{item.Codigo} - {item.Descricao}");
+        }
+        */
+
+        /*
+        foreach (Regiao item in contexto.Regioes)
+        {
+            Console.WriteLine($"{item.CodigoRegiao} - {item.Nome}");
+        }
+        */
+
+        /*
+        foreach (Estado item in contexto.Estados)
+        {
+            Console.WriteLine($"{item.CodigoEstado} - {item.Nome}");
+        }
+        */
+
+        foreach (Cidade item in contexto.Cidades)
+        {
+            Console.WriteLine($"{item.CodigoCidade} - {item.Nome}");
+        }
+
+        Console.ReadLine();
+    }
+}
